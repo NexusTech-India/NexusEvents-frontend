4ca4edaceee36f31faecfdcbaaeab8f1029dee2b
SudhanPlayz <sudhanplayz@gmail.com>
2023-12-09 16:25:12 +0000

    cf ipfs

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -40,7 +40,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     );
 
     // Values for the verifier
-    const sLoader = new loaders.UniversalSchemaLoader("ipfs.io");
+    const sLoader = new loaders.UniversalSchemaLoader("cloudflare-ipfs.com");
 
     // Initialize the verifier with the values described above.
     const verifier = new auth.Verifier(verificationKeyloader, sLoader, resolvers);
