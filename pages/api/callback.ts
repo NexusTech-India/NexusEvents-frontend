1d645390ccd33d825cfd26c71ec2e9aa2dfc2524
SudhanPlayz <sudhanplayz@gmail.com>
2023-12-09 09:49:26 +0000

    a

---
+++ pages/api/callback.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "@/server/db"
+import { auth, resolver } from "@iden3/js-iden3-auth";
+import path from "path"
+import getRawBody from "raw-body";
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    let { sessionId } = req.query;
+    sessionId = sessionId as string;
+    const session = await db.session.findUnique({ where: { id: sessionId } });
+    if (!session) return res.status(404).send("Session not found");
+
+    // get JWZ token params from the post request
+    const raw = await getRawBody(req);
+    const tokenStr = raw.toString().trim();
+
+    const ethURL = 'https://polygon-mumbai.g.alchemy.com/v2/ZPG4ZM3lIzQubp0v9-kGBtiRcLJ2oX2L';
+    const contractAddress = "0x134B1BE34911E39A8397ec6289782989729807a4"
+
+    const ethStateResolver = new resolver.EthStateResolver(
+        ethURL,
+        contractAddress,
+    );
+
+    const resolvers = {
+        ['polygon:mumbai']: ethStateResolver,
+    };
+
+    // EXECUTE VERIFICATION
+    const verifier = await auth.Verifier.newVerifier(
+        {
+            stateResolver: resolvers,
+            circuitsDir: path.join(__dirname, "..", "..", "keys"),
+            ipfsGatewayURL: "https://ipfs.io"
+        }
+    );
+
+    let authResponse;
+
+    try {
+        const opts = {
+            AcceptedStateTransitionDelay: 5 * 60 * 1000, // 5 minute
+        };
+        authResponse = await verifier.fullVerify(tokenStr, JSON.parse(session.request!.toString()), opts as any)
+        await db.session.update({ where: { id: sessionId }, data: { response: authResponse as any } })
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+    return res.status(200).send("user with ID: " + authResponse.from + " Succesfully authenticated");
+}
---
+++ pages/api/sign-in.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "@/server/db"
+import { auth } from "@iden3/js-iden3-auth";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const data = await db.session.create({ data: {} })
+    const { id } = data;
+
+    const hostUrl = "https://nexus-events.vercel.app";
+    const callbackURL = "/api/callback"
+    const audience = "did:polygonid:polygon:mumbai:2qKMLW2WxXWmTMLiAEuWmBcbzdVYyTWAL6MtceJJvF"
+
+    const uri = `${hostUrl}${callbackURL}?sessionId=${id}`;
+
+    // Generate request for basic authentication
+    const request = auth.createAuthorizationRequest(
+        'test flow',
+        audience,
+        uri,
+    );
+
+    request.id = id;
+    request.thid = id;
+
+    // Add request for a specific proof
+    const proofRequest = {
+        id: 1,
+        circuitId: 'credentialAtomicQuerySigV2',
+        query: {
+            allowedIssuers: [audience],
+            type: 'Verification',
+            context: 'ipfs://QmZJo92wz58RNq9kWS8vwXMxNiVs4TgwjVbHv87FYXS6oQ',
+            credentialSubject: {
+                birthday: {
+                    $lt: 20100101,
+                },
+            },
+        },
+    };
+    const scope = request.body.scope ?? [];
+    request.body.scope = [...scope, proofRequest];
+
+    data.request = request;
+    await db.session.update({ where: { id }, data: { request } });
+
+    return res.status(200).send(request);
+}
